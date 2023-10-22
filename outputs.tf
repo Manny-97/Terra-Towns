437908b11e693752746cd25aad1bc79956985fcb
Muhammed Jimoh <Muhammedjimoh47@gmail.com>
2023-10-22 10:21:18 +0000

    feat: implement uploading page file to s3

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "bucket_name" {
   description = "S3 bucket name for our static website."
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+    value = module.terrahouse_aws.website_endpoint
+}
