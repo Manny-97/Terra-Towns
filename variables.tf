f83c02a2aea56a4c5e64402733814a763c32d380
Muhammed Jimoh <Muhammedjimoh47@gmail.com>
2023-11-26 18:00:21 +0000

    feat: deploying to terratowns

--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
+variable "terratowns_endpoint" {
+ type = string
+}
+
+variable "terratowns_access_token" {
+ type = string
+}
+
+variable "teacherseat_user_uuid" {
+ type = string
+}
 variable "user_uuid" {
     type = string
 }
