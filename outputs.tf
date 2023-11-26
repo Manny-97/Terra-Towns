9fa18694b8306a7141749c5d2fae2cad279d8b93
Muhammed Jimoh <64415804+Manny-97@users.noreply.github.com>
2023-11-26 13:31:38 +0100

    38 terratowns provider (#40)
    * feat: setup skeleton for terraform provider
    
    * feat: show logs of tf, setup provider in main.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "bucket_name" {
-  description = "S3 bucket name for our static website."
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "S3 bucket name for our static website."
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-    value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#     value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cdn_domain" {
-  value = module.terrahouse_aws.website_domain
-}
+# output "cdn_domain" {
+#   value = module.terrahouse_aws.website_domain
+# }
